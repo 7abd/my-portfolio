364daa2fe0a1692922f5abb19161aa7c9f695ca8
abdennour darkaoui <abdobaouch996@gmail.com>
2026-04-27 21:45:03 +0100

    Clean up console logs in route.js
    Removed console logs for API key and from email.

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -3,11 +3,6 @@ import { NextResponse } from "next/server";
 import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail = process.env.FROM_EMAIL;
-console.log('dkh')
-
-console.log("API KEY:", process.env.RESEND_API_KEY);
-console.log("FROM EMAIL:", process.env.FROM_EMAIL);
-
 
 export async function POST(req, res) {
    const body = await req.json();
